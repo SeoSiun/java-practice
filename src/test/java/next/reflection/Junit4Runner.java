93f8362505e7186f631387e4f385874a31a6227b
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-12 18:05:49 +0900

    요구사항 3 - @Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -1,12 +1,24 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit4Runner {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
     @Test
     public void run() throws Exception {
         Class clazz = Junit4Test.class;
+        Method[] methods = clazz.getDeclaredMethods();
 
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
 
