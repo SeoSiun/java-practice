396bfd2b973535f095ef71ea6a997a98d891752a
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-12 18:05:32 +0900

    요구사항 2 - test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -1,11 +1,24 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit3Runner {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
     @Test
     public void runner() throws Exception {
         Class clazz = Junit3Test.class;
+        Method[] methods = clazz.getDeclaredMethods();
 
+        for (Method method : methods) {
+            logger.debug(method.getName());
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
